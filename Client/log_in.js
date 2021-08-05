ed199941bb77768277783d7a9c409e038bd810b2
sbgsm <78792156+sbgsm@users.noreply.github.com>
2021-08-05 16:00:40 +0800

    Add files via upload

---
+++ Client/log_in.js
@@ -0,0 +1,15 @@
+function send_user_login() {
+    var username = document.getElementById("username");
+    var password = document.getElementById("password");
+    if (username.value == "") {
+        alert("请输入用户名");
+        username.focus();
+        return false;
+    }
+    else if (password.value == "") {
+        alert("请输入密码");
+        password.focus();
+        return false;
+    }
+    document.getElementById("form1").submit();
+}
