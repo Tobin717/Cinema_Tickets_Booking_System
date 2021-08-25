505eeeea431e893797d7cda5cffd56d31e939e22
qsz <928686953@qq.com>
2021-08-25 21:57:07 +0800

    up load

--- Client/js/check_cookie.js
+++ Client/js/check_cookie.js
@@ -32,6 +32,6 @@ function clearAllCookie() {
     var keys = document.cookie.match(/[^ =;]+(?=\=)/g);
     if (keys) {
         for (var i = keys.length; i--;)
-            document.cookie = keys[i] + '=0;expires=' + new Date(0).toUTCString()
+            document.cookie = keys[i] + '=0;expires=' + new Date(0).toUTCString();
     }
 }
