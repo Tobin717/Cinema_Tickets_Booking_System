b49f5614188b889740ed1cff0e620f4984706991
Tobin717 <raozhongtao@iclou,com>
2021-08-29 21:06:13 +0800

    fix bugs

--- Server/database.py
+++ Server/database.py
@@ -48,30 +48,29 @@ def getUnavailableFilm(film_id):
 	return result
 
 def book(userid,film_id,seats,number):
+	for i in range(0,number):
+		db.execute('SELECT balance FROM userinfo WHERE `userid`=%s', (userid,))
+		balance=db.fetchone()['balance']
+		db.execute('SELECT userid FROM tickets WHERE film_id=%s and row=%s and col=%s',(film_id,seats[i]['row'],seats[i]['col']))
+		if db.rowcount>0:
+			conn.rollback()
+			return -2
+		if balance<50:
+			conn.rollback()
+			return -1
 	try:
-		for i in range(0,number):
-			db.execute('insert into tickets (`film_id`,`userid`,`row`,`col`) values(%s,%s,%s,%s)',(film_id,userid,seats[i]['row'],seats[i]['col']))
-			db.execute('select balance from userinfo where `userid`=%s', (userid,))
-			balance=db.fetchone()['balance']
-			db.execute('SELECT userid FROM tickets WHERE film_id=%s,row=%s,col=%s',(film_id,seats[i]['row'],seats[i]['col']))
-			exist=db.fetchone()
-			if exist:
-				conn.rollback()
-				return -2
-			if balance<50:
-				conn.rollback()
-				return -1
-			else:
-				balance=balance-50
-				db.execute('UPDATE userinfo SET balance=%s WHERE userid=%s',(balance,userid))
+		balance=balance-50
+		db.execute('UPDATE userinfo SET balance=%s WHERE userid=%s',(balance,userid))
+		db.execute('insert into tickets (`film_id`,`userid`,`row`,`col`) values(%s,%s,%s,%s)',(film_id,userid,seats[i]['row'],seats[i]['col']))
 	except Exception as e:
 		conn.rollback()
 		print(e)
 	finally:
 		conn.commit()
-	return db.rowcount
+		return db.rowcount
 
 def getUserTickets(userid):
+	result={'film_id':'','cinema_name':'','mv_name':'','hall_id':'','start_time':''}
 	db.execute('select film_id,row,col from tickets where `userid`=%s', (userid,))
 	result=db.fetchall()
 	return result
@@ -116,6 +115,7 @@ def searchByName(mv_name):
 	result=[]
 	db.execute('SELECT film_id,cinema_id,hall_id,mv_name,start_time FROM filmsession WHERE mv_name=%s',(mv_name,))
 	dbresult=db.fetchall()
+	number=len(dbresult)
 	#print(dbresult)
 	for i in range(0,number):
 		temp={'film_id':'','cinema_name':'','mv_name':'','hall_id':'','start_time':''}
@@ -126,7 +126,7 @@ def searchByName(mv_name):
 		temp['hall_id']=dbresult[i]['hall_id']
 		temp['film_id']=dbresult[i]['film_id']
 		result.append(temp)
-	return result
+	return result,number
 
 def encrypt(passwd):
     return generate_password_hash(passwd)
